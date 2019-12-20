bbb04c70ce0791f3bf58779b06ffbdf9ddcf443f
Sameeha Rahman <rahman.sameeha@gmail.com>
2019-12-21 00:18:51 +0530

    Complete filtering

--- eat-smeeshy/src/App.js
+++ eat-smeeshy/src/App.js
@@ -5,14 +5,65 @@ import TopNavBar from './components/reusable/Navbar';
 import Footer from './components/reusable/Footer';
 import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <TopNavBar />
-      <Home />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      allRest: [],
+      filteredRest: [],
+      userPos: {},
+      rateFilterValue: 0,
+      priceFilterValue: 4
+    };
+  }
+
+  componentDidMount() {
+    this.setRestaurants();
+    this.setUserLocation();
+  }
+
+  setRestaurants() {
+    fetch("http://localhost:5000/restaurants/")
+      .then(response => response.json())
+      .then(restaurantList => {
+        this.setState({ allRest: restaurantList, filteredRest: restaurantList });
+      }).catch(error => console.error(error));
+  }
+
+  setUserLocation() {
+    navigator.geolocation.getCurrentPosition(currPos => {
+      this.setState({ userPos: { lat: currPos.coords.latitude, lng: currPos.coords.longitude } });
+    }, (error) => {
+      console.error(error);
+    }, { timeout: 20000 });
+  }
+
+  changePrice(priceVal) {
+    console.log("priceVal", priceVal);
+    this.setState({ priceFilterValue: priceVal }, () => this.filterRestaurants(this.state.priceFilterValue, this.state.rateFilterValue));
+  }
+
+  changeRate(rateVal) {
+    console.log("rateVal", rateVal);
+    this.setState({ rateFilterValue: rateVal }, () => this.filterRestaurants(this.state.priceFilterValue, this.state.rateFilterValue));
+  }
+
+  filterRestaurants(priceVal, rateVal) {
+
+    let filteredList = this.state.allRest.filter(rest => rest.price_level <= priceVal && rest.rating >= rateVal);
+    this.setState({ filteredRest: filteredList });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TopNavBar onPriceChange={this.changePrice.bind(this)} onRateChange={this.changeRate.bind(this)} />
+        <Home userPos={this.state.userPos} allRest={this.state.filteredRest} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- eat-smeeshy/src/components/reusable/navbar.js
+++ eat-smeeshy/src/components/reusable/navbar.js
@@ -1,32 +1,58 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 import logo from '../../assets/images/logo.svg';
 
-function TopNavBar() {
-    return (
-        <Navbar variant="dark" fixed="top">
-            <Navbar.Brand href="#home">
-                <img
-                    alt=""
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />{' '}
-                Eat Smeeshy!
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Form inline className="ml-auto">
-                    <FormControl type="text" placeholder="Search restaurants" className="mr-sm-2" />
-                    <Button variant="outline-light">Search</Button>
-                </Form>
-            </Navbar.Collapse>
-        </Navbar>
-    )
+class TopNavBar extends React.Component {
+    handlePriceChange = (e) => {
+        let { onPriceChange } = this.props;
+        onPriceChange(parseInt(e.target.value));
+    };
+
+    handleRateChange = (e) => {
+        let { onRateChange } = this.props;
+        onRateChange(parseInt(e.target.value));
+    };
+
+    render() {
+        return (
+            <Navbar collapseOnSelect expand="lg" variant="dark" fixed="top">
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    Eat Smeeshy!
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsiveNavbar" />
+                <Navbar.Collapse id="responsiveNavbar" >
+                    <Nav />
+                    <Form inline className="ml-auto" >
+                        <Navbar.Text className="mr-sm-2">Filter restaurants by</Navbar.Text>
+                        <Form.Control ref={rateSelect => this.rateSelect} as="select" id="starRateVal" className="mr-sm-2" onChange={this.handleRateChange}>
+                            <option value={0}>All ratings</option>
+                            <option value={1}>+1 star</option>
+                            <option value={2}>+2 stars</option>
+                            <option value={3}>+3 stars</option>
+                            <option value={4}>+4 stars</option>
+                            <option value={5}>5 stars</option>
+                        </Form.Control>
+                        <Form.Control ref={priceSelect => this.priceSelect} as="select" id="priceRange" onChange={this.handlePriceChange} >
+                            <option value={4}>$$$$</option>
+                            <option value={3}>$$$</option>
+                            <option value={2}>$$</option>
+                            <option value={1}>$</option>
+                        </Form.Control>
+                        {/* <FormControl type="text" placeholder="Search restaurants" className="mr-sm-2" /> */}
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
 }
 
 export default TopNavBar;
